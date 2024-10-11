91e8518b8212281b6e32873c7e9c7830d929ea54
Lissa0628 <69712197+Lissa0628@users.noreply.github.com>
2024-10-11 17:16:46 +0700

    Задание 2 выполнили leonova и shekh

---
+++ leonovashekh2/create_project.sh
@@ -0,0 +1,6 @@
+   project_name="leonovashekh2"
+mkdir -p "$project_name/src/{scripts,styles,images}"
+touch "$project_name/README.md"
+touch "$project_name/.gitignore"
+echo "Структура проекта '$project_name' успешно создана."
+
